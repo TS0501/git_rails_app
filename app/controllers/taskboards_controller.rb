a97687b77376756a784f4f342fd0740aff321924
Taichi Shibuya <taichishibuya@TaichinoMacBook-Pro.local>
2020-06-25 14:39:03 +0900

    taskboardsコントローラーに削除機能追加

--- app/controllers/taskboards_controller.rb
+++ app/controllers/taskboards_controller.rb
@@ -12,4 +12,14 @@ class TaskboardsController < ApplicationController
 
   def edit
   end
+
+  def destroy
+    taskboard = Taskboard.find(params[:id])
+
+    if taskboard.destroy
+      redirect_to taskboards_url, notice: "タスク「#{taskboard.name}」を削除しました。"
+    else
+      redirect_to taskboards_url, alert: "削除できません"
+    end
+  end
 end
